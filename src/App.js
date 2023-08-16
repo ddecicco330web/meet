17d57c9b87b84e5c20565e57660164120643fe32
ddecicco330web <ddecicco330@gmail.com>
2023-08-16 14:37:30 -0400

    Implement integration testing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import './App.css';
+import { useEffect, useState } from 'react';
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
 import NumberOfEvents from './components/NumberOfEvents';
+import { getEvents, extractLocations } from './api';
+
+import './App.css';
 
 const App = () => {
+  const [events, setEvents] = useState([]);
+  // eslint-disable-next-line no-unused-vars
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState('See all cities');
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents =
+      currentCity === 'See all cities'
+        ? allEvents
+        : allEvents.filter((event) => event.location === currentCity);
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [currentCity]);
+
   return (
     <div className="App">
-      <CitySearch />
-      <EventList />
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+      <EventList events={events} />
       <NumberOfEvents />
     </div>
   );
