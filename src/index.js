2f665fbc25ccb2bb36974152ad14e50c353edae2
ddecicco330web <ddecicco330@gmail.com>
2023-08-25 12:35:53 -0400

    implement atatus performace monitoring

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('3257942521fa4d4998921540d995f72b').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
