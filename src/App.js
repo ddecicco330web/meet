6362aa40be272543a1752410e1be9a86fc759e1c
ddecicco330web <ddecicco330@gmail.com>
2023-09-19 21:03:04 -0400

    Add pie chart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NumberOfEvents from './components/NumberOfEvents';
 import { getEvents, extractLocations } from './api';
 import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 import CityEventsChart from './components/CityEventsChart';
+import EventGenresChart from './components/EventGenresChart';
 
 import './App.css';
 
@@ -52,7 +53,10 @@ const App = () => {
         setCurrentNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert}
       />
-      <CityEventsChart allLocations={allLocations} events={events} />
+      <div className="charts-container">
+        <EventGenresChart events={events} />
+        <CityEventsChart allLocations={allLocations} events={events} />
+      </div>
       <EventList events={events} />
     </div>
   );
--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -17,14 +17,14 @@ const CityEventsChart = ({ allLocations, events }) => {
   }, [`${events}`]);
 
   const getData = () => {
-    const data = allLocations.map((location) => {
+    const locationData = allLocations.map((location) => {
       const count = events.filter(
         (event) => event.location === location
       ).length;
       const city = location.split(/, | - /)[0];
       return { city, count };
     });
-    return data;
+    return locationData;
   };
 
   return (
